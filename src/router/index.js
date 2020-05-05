ba43518899ab6cf44ebb848bde2f9f6d0bb3a484
Mohit Chandana <chandanamohit@gmail.com>
2020-05-06 02:00:23 +0530

    create gmap component and make it home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import GMap from '@/components/home/GMap.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'GMap',
+    component: GMap
   },
   {
     path: '/about',
